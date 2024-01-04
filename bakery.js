bc16ebeb124e86eb6c52875ab43c838127787fb8
manginoja <36826929+manginoja@users.noreply.github.com>
2024-01-03 20:50:32 -0800

    Update bakery.js

--- bakery.js
+++ bakery.js
@@ -1,4 +1,4 @@
-import * as THREE from './node_modules/three/build/three.module.js';
+import * as THREE from 'https://github.com/manginoja/boulangerie/blob/master/node_modules/three/build/three.module.js';
 /*
 import { OrbitControls }  from './node_modules/three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from './node_modules/three/examples/jsm/loaders/GLTFLoader.js';
